daeb1273a92222606b1f376335ebc442fe611e09
pranav singh chauhan <ps8928969@gmail.com>
2025-11-20 22:26:43 +0530

    initial commit: c++ basics programs

---
+++ compairing 2 integers in c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+      int main() {
+        int a , b;
+        cout << "Enter two integers: "<< endl;
+        cin >> a >> b;
+        cout << "on compariring a and b a=b"<< endl<< (a==b) << endl;
+        cout << "on compariring a and b a!=b"<< endl<< (a!=b) << endl;
+        cout << "on compariring a and b a>b"<< endl<< (a>b) << endl;
+        cout << "on compariring a and b a<b"<< endl << (a<b) << endl;
+        cout << "on comparing a and b a>=b"<< endl << (a>=b) << endl;
+        cout << "on comparing a and b a<=b"<< endl<< (a<=b) << endl;
+
+        return 0;
+      }
